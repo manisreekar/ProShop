b6d6e7eb4abb8c8324c815a1c5a6d11a025c1282
Mani Sreekar Palakurthy <manisreekarpalakurthy@Manis-MacBook-Air.local>
2024-02-26 12:58:25 -0700

    created backend product controller and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import router from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import router from './routes/productRoutes.js';
 dotenv.config();
 
 
@@ -14,5 +15,8 @@ res.send("API is running");
 });
 
 app.use('/api/products',router)
+
+app.use(notFound);
+app.use(errorHandler);
  
 app.listen(port,()=>console.log(`Server running on port ${port}`))
